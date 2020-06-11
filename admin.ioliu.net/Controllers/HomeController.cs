6ba535a4019c1654afe50085ad90c6ab30a157fa
天苍苍兮 <576660756@qq.com>
2020-06-11 17:38:55 +0800

    efcore

--- admin.ioliu.net/Controllers/HomeController.cs
+++ admin.ioliu.net/Controllers/HomeController.cs
@@ -6,21 +6,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using admin.ioliu.net.Models;
+using Microsoft.EntityFrameworkCore;
+using ioliu.Data;
+using ioliu.Domain;
 
 namespace admin.ioliu.net.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IoliuContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IoliuContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var user = _context.systemUsers.Where(i=>i.UserName=="df").ToList();
+            if (user.Count == 0) { 
+            SystemUsers systemUsers = new SystemUsers
+            {
+                Id = Guid.NewGuid(),
+                UserName="df",
+                PassWord="df727123."
+            };
+            }
+            var df = _context.systemUsers.ToList();
+            
+            return View(df);
         }
 
         public IActionResult Privacy()
---
+++ ioliu.Domain/SystemUsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ioliu.Domain
+{
+   public class SystemUsers
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
